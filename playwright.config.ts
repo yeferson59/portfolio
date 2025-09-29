75ad29ae37dc9d26de2be66d47132bcfa564010c
Yeferson Toloza <116679774+yeferson59@users.noreply.github.com>
2025-09-28 23:46:22 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
+    command: 'bun run preview',
     url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
